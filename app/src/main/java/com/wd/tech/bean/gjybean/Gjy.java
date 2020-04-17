180d73d5796c26338ce7900d49177fb62340772b
huanyingyuequezuijihe <1165413404@qq.com>
2020-04-17 19:42:29 +0800

    0417-bean开始

---
+++ app/src/main/java/com/wd/tech/bean/gjybean/Gjy.java
@@ -0,0 +1,11 @@
+package com.wd.tech.bean.gjybean;
+
+/**
+ * @author 王阳
+ * Class :1708A
+ * @description:
+ * @date :2020/4/17 19:41
+ * @classname :Gjy
+ */
+public class Gjy {
+}
---
+++ app/src/main/java/com/wd/tech/bean/qzjbean/Qzj.java
@@ -0,0 +1,11 @@
+package com.wd.tech.bean.qzjbean;
+
+/**
+ * @author 王阳
+ * Class :1708A
+ * @description:
+ * @date :2020/4/17 19:41
+ * @classname :Gjy
+ */
+public class Qzj {
+}
