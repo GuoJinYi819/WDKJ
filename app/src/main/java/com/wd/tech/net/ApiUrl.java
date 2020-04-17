93c0c014d616536d061e9371466b3fb046bdf6c8
huanyingyuequezuijihe <1165413404@qq.com>
2020-04-17 21:40:29 +0800

    0417-ApiUrl初步创建

--- app/src/main/java/com/wd/tech/net/ApiUrl.java
+++ app/src/main/java/com/wd/tech/net/ApiUrl.java
@@ -8,5 +8,15 @@ package com.wd.tech.net;
  * @classname :ApiUrl
  */
 public interface ApiUrl {
-    String URL="http://mobile.bwstudent.com/techApi/";
+    String URL="https://mobile.bwstudent.com/techApi/";
+    //登录
+    String LOGIN_URL="user/v1/login";
+    //注册
+    String REGISTER_URL="user/v1/register";
+    //咨询页 xbanner
+    String CONSULTATION_XBANNER_URL="information/v1/bannerShow";
+    //消息首页
+    String NEWS_URL="";
+    //社区首页
+    String COMMUNITY_URL="";
 }
