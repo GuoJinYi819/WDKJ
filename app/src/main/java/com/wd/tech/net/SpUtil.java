5b855e52db38857a2c4f087d4149b369e7266354
1708Ag <1708Ag>
2020-04-18 10:42:53 +0800

    新版ShardPreference

--- app/src/main/java/com/wd/tech/net/SpUtil.java
+++ app/src/main/java/com/wd/tech/net/SpUtil.java
@@ -13,38 +13,52 @@ import com.wd.tech.App;
  * @classname :SpUtil
  */
 public class SpUtil {
+
+    private static SpUtil instance;
+    private final SharedPreferences user;
+    private final SharedPreferences.Editor edit;
+
+    private SpUtil(){
+        user = App.context.getSharedPreferences("user", Context.MODE_PRIVATE);
+        edit = user.edit();
+    }
+
+    public static SpUtil getInstance(){
+        if (instance==null) {
+            synchronized (SpUtil.class){
+                if (instance == null) {
+                    instance = new SpUtil();
+                }
+            }
+        }
+        return instance;
+    }
+
     //缓存
-    public static void saveString(String key,String value){
-        SharedPreferences user = App.context.getSharedPreferences("user", Context.MODE_PRIVATE);
-        SharedPreferences.Editor edit = user.edit();
+    public void saveString(String key,String value){
+
         edit.putString(key, value);
         edit.commit();
     }
-    public static void saveInt(String key,int value){
-        SharedPreferences user = App.context.getSharedPreferences("user", Context.MODE_PRIVATE);
-        SharedPreferences.Editor edit = user.edit();
+    public void saveInt(String key,int value){
+
         edit.putInt(key, value);
         edit.commit();
     }
-    public static void saveBoolean(String key,boolean value){
-        SharedPreferences user = App.context.getSharedPreferences("user", Context.MODE_PRIVATE);
-        SharedPreferences.Editor edit = user.edit();
+    public void saveBoolean(String key,boolean value){
         edit.putBoolean(key, value);
         edit.commit();
     }
     //取缓存
-    public static String getSpString(String key){
-        SharedPreferences user = App.context.getSharedPreferences("user", Context.MODE_PRIVATE);
+    public String getSpString(String key){
         String string = user.getString(key, "");
         return string;
     }
-    public static int getSpInt(String key){
-        SharedPreferences user = App.context.getSharedPreferences("user", Context.MODE_PRIVATE);
+    public int getSpInt(String key){
         int anInt = user.getInt(key, -1);
         return anInt;
     }
-    public static boolean getSpBoolean(String key){
-        SharedPreferences user = App.context.getSharedPreferences("user", Context.MODE_PRIVATE);
+    public boolean getSpBoolean(String key){
         boolean aBoolean = user.getBoolean(key, false);
         return aBoolean;
     }
