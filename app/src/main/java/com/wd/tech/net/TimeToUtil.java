236a15dcdfff0020ffdf199ade2cfc5420b7e1ab
huanyingyuequezuijihe <1165413404@qq.com>
2020-04-19 13:27:04 +0800

    04-19-时间戳工具类

---
+++ app/src/main/java/com/wd/tech/net/TimeToUtil.java
@@ -0,0 +1,20 @@
+package com.wd.tech.net;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author 王阳
+ * Class :1708A
+ * @description:
+ * @date :2020/4/19 13:25
+ * @classname :TimeToUtil
+ */
+public class TimeToUtil {
+    //时间戳工具类
+    public static String getTime(long time){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh-MM-ss");
+        String format = simpleDateFormat.format(new Date(time));
+        return format;
+    }
+}
