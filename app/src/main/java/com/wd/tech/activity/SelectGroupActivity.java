b7cc34540a944262cd606c0cfa323c00aead2d33
GuoJinYi819 <gjy2000@>
2020-04-25 22:09:35 +0800

    04-25:消息模块-展示所有分组

--- app/src/main/java/com/wd/tech/activity/SelectGroupActivity.java
+++ app/src/main/java/com/wd/tech/activity/SelectGroupActivity.java
@@ -6,9 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
@@ -16,14 +21,25 @@ import com.wd.tech.R;
 import com.wd.tech.base.BaseActivity;
 import com.wd.tech.base.BasePresenter;
 import com.wd.tech.bean.gjybean.AddFriendGroupBean;
+import com.wd.tech.bean.gjybean.FriendGroupListBean;
 import com.wd.tech.mvp.gjymvp.addfriendgroup.AddFriendGroupPresenter;
 import com.wd.tech.mvp.gjymvp.addfriendgroup.IAddFriendGroupContract;
+import com.wd.tech.net.ApiService;
+import com.wd.tech.net.RetrofitUtil;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class SelectGroupActivity extends BaseActivity<AddFriendGroupPresenter> implements IAddFriendGroupContract.IAddFriendGroupView {
 
     private android.widget.ImageView mIvBlac;
     private android.widget.TextView mTvAdd;
     private android.widget.ListView mListView;
+    private AlertDialog alertDialog;
 
     @Override
     public int initLayout() {
@@ -46,7 +62,7 @@ public class SelectGroupActivity extends BaseActivity<AddFriendGroupPresenter> i
                 AlertDialog.Builder dialog = new AlertDialog.Builder(SelectGroupActivity.this);
                 View view = View.inflate(SelectGroupActivity.this, R.layout.dialog_view, null);
                 dialog.setView(view);
-                AlertDialog alertDialog = dialog.create();
+                alertDialog = dialog.create();
                 EditText edContent = view.findViewById(R.id.edContent);
                 Button btnQ = view.findViewById(R.id.btnQ);
                 Button btnC = view.findViewById(R.id.btnC);
@@ -76,6 +92,8 @@ public class SelectGroupActivity extends BaseActivity<AddFriendGroupPresenter> i
         Intent intent = getIntent();
         int groupId = intent.getIntExtra("groupId", -1);
         int friend = intent.getIntExtra("friend", -1);
+        showListView();
+
     }
 
     @Override
@@ -86,11 +104,59 @@ public class SelectGroupActivity extends BaseActivity<AddFriendGroupPresenter> i
     @Override
     public void onAddFriendGroupSuccess(AddFriendGroupBean bean) {
         String message = bean.getMessage();
-        Toast.makeText(this, ""+message, Toast.LENGTH_SHORT).show();
+        if (message.equals("创建分组成功")) {
+            showListView();
+            alertDialog.dismiss();
+        }
+
     }
 
     @Override
     public void onFailed(String error) {
 
+    }
+
+    private void showListView(){
+        RetrofitUtil instance = RetrofitUtil.getInstance();
+        ApiService apiService = instance.createService();
+        Observable<FriendGroupListBean> observable = apiService.getFriendGroupList();
+        observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<FriendGroupListBean>() {
+                    @Override
+                    public void accept(FriendGroupListBean friendGroupListBean) throws Exception {
+                        String message = friendGroupListBean.getMessage();
+                        if (message.equals("查询成功")) {
+                            List<FriendGroupListBean.ResultBean> list = friendGroupListBean.getResult();
+                            mListView.setAdapter(new BaseAdapter() {
+                                @Override
+                                public int getCount() {
+                                    return list.size();
+                                }
+
+                                @Override
+                                public Object getItem(int position) {
+                                    return list.get(position);
+                                }
+
+                                @Override
+                                public long getItemId(int position) {
+                                    return position;
+                                }
+
+                                @Override
+                                public View getView(int position, View convertView, ViewGroup parent) {
+                                    View inflate = View.inflate(SelectGroupActivity.this, R.layout.item_groupwhole, null);
+                                    TextView tvGroupName = inflate.findViewById(R.id.tvGroupName);
+                                    FriendGroupListBean.ResultBean resultBean = list.get(position);
+                                    String groupName = resultBean.getGroupName();
+                                    tvGroupName.setText(groupName);
+                                    return inflate;
+                                }
+
+                            });
+                        }
+                    }
+                });
     }
 }
